6f79b2f83107c724ec3f0c6b512655a2ce9921ac
Connor <heidema.connor@gmail.com>
2020-06-20 16:07:11 -0700

    more stuff

--- src/system/GameRenderSys.cpp
+++ src/system/GameRenderSys.cpp
@@ -30,7 +30,7 @@ void GameRenderSys::Update(entt::registry& reg, std::shared_ptr<sf::RenderWindow
 	reg.view<PositionComp, RenderableComp>().each([pRenderWindow](auto entity, auto &posComp)
 	{
 		// dummy data for now
-		auto a = sf::RectangleShape(sf::Vector2f(20,20));
+		auto a = sf::RectangleShape(sf::Vector2f(20, 20));
 		a.setFillColor(sf::Color::Green);
 		a.setPosition(posComp.position.x, posComp.position.y);
 		pRenderWindow->draw(a);
